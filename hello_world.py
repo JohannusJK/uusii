f98756b0985e30cd86113433592efa5431b41a86
Johannes <Johannes.Korpimies@tuni.fi>
2022-10-30 18:59:32 +0200

    Add prints

--- hello_world.py
+++ hello_world.py
@@ -1,1 +1,10 @@
 print("Hello world!")
+print("Not another hello world")
+print("No")
+print("More")
+print("Hello")
+print("World")
+<<<<<<< HEAD
+print("Please, I'm begging you")
+=======
+>>>>>>> c3cf8d5 (qwerty)
